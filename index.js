8dda570a89db077f41eda6cdcce45a774fe36390
Ian <user.emai>
2024-04-03 13:10:38 +0300

    DomModifyingElementsLab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+//write your code here
+main.remove()
+
+//newHeader
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Ian is the champion"
